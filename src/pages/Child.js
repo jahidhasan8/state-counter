ae3e2c191331f3ef1137f301dfa6447fddd2798d
jahidhasan8 <jahidhasan1932@gmail.com>
2023-02-11 02:00:33 +0600

    modified

--- src/pages/Child.js
+++ src/pages/Child.js
@@ -7,8 +7,8 @@ const Child = () => {
             <div>
                 <h1>{count}</h1>
                 <div>
-                    <button>Decrement</button>
-                    <button>Increment</button>
+                    <button onClick={()=>setCount(count-1)}>Decrement</button>
+                    <button onClick={()=>setCount((prvState)=>prvState+1)}>Increment</button>
                 </div>
             </div>
         </div>
@@ -16,3 +16,46 @@ const Child = () => {
 };
 
 export default Child;
+
+
+
+// import React, { useState } from "react";
+
+// //* Single State (useState)
+
+// function LessonOne() {
+//   const [count, setCount] = useState(0);
+
+//   const delay = () => {
+//     setTimeout(() => {
+//       setCount((current) => current + 1);
+//     }, 2000);
+//   };
+
+//   return (
+//     <div className='w-full max-w-xl bg-gradient-to-tr to-purple-100 from-cyan-100 p-2 rounded-md shadow-2xl'>
+//       <div className='bg-white rounded-md p-10'>
+//         <h1 className='text-2xl text-center mb-5'>{count}</h1>
+//         <div className='flex justify-center gap-5'>
+//           <button
+//             onClick={() => setCount((current) => current - 1)}
+//             className='py-2 px-3 bg-purple-200 rounded-md '>
+//             Decrement
+//           </button>
+//           <button
+//             onClick={() => setCount((current) => current + 1)}
+//             className='py-2 px-3 bg-cyan-200 rounded-md '>
+//             Increment
+//           </button>
+//           <button
+//             onClick={delay}
+//             className='py-2 px-3 bg-green-400 rounded-md '>
+//             Async Increment
+//           </button>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default LessonOne;
